3f795849766b52995d2a5a2028a0040a2e8f5253
YashPatel166 <60495318+YashPatel166@users.noreply.github.com>
2023-12-07 13:15:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,27 +7,50 @@
 #include <stack>
 #include <set> 
 #include <thread>
-#include <chrono>
+#include <ctime>
 #include <vector>
+#include <chrono>
+#include <cstdlib>
+#include <sys/time.h>
 #include "/usr/local/opt/libomp/include/omp.h"
 
 int main()
 {
+
+ struct timeval startTime, stopTime;
+ gettimeofday(&startTime, nullptr);
+
+   // clock_t start = clock();
+
+
+ 
     AStar::Generator generator;
-    generator.setWorldSize({15, 15});
+    generator.setWorldSize({1000, 1000});
     generator.setHeuristic(AStar::Heuristic::euclidean);
-    generator.setDiagonalMovement(true);
+   
     std::cout << "Generate path ... \n";
     int x1, y1, x2, y2;
     std::cout<<"Enter X1 and Y1:\n";
     std::cin>>x1>>y1;
     std::cout<<"Enter X2 and Y2:\n";
     std::cin>>x2>>y2;
-    generator.ZobristTable(x1, y1, x2, y2, 100);
+    
     auto path = generator.findPath({x1,y1}, {x2,y2});
 
     for(auto& coordinate : path) {
         std::cout << "("<< coordinate.x << "," << coordinate.y << ") -> ";
     }
-     std::cout<<"\n";
+
+
+gettimeofday(&stopTime, nullptr);
+    double duration = (stopTime.tv_sec - startTime.tv_sec) + (stopTime.tv_usec - startTime.tv_usec) / 1e6;
+
+    std::cout << "Time taken by function: " << duration << " seconds\n";
+
+   /*clock_t stop = clock();
+  
+    double duration = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    std::cout << "Time taken by function: " << duration << " seconds\n";
+     std::cout<<"\n";*/
 }
