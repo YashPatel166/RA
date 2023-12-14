ef4e4945227dacdea8576409e52cc00b2d282b37
YashPatel166 <60495318+YashPatel166@users.noreply.github.com>
2023-12-14 12:06:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,23 +9,22 @@
 #include <thread>
 #include <ctime>
 #include <vector>
-#include <chrono>
 #include <cstdlib>
 #include <sys/time.h>
 #include "/usr/local/opt/libomp/include/omp.h"
 
 int main()
 {
-
- struct timeval startTime, stopTime;
- gettimeofday(&startTime, nullptr);
-
-   // clock_t start = clock();
-
-
  
     AStar::Generator generator;
-    generator.setWorldSize({1000, 1000});
+    auto start = std::chrono::high_resolution_clock::now();
+    generator.setWorldSize({10000, 10000});
+    generator.initializeConstants();
+    generator.setDiagonalMovement(false);
+     auto stop = std::chrono::high_resolution_clock::now();
+	double duration1 = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
+
+    std::cout<<"Time taken for initialization:"<<duration1<<"\n";
     generator.setHeuristic(AStar::Heuristic::euclidean);
    
     std::cout << "Generate path ... \n";
@@ -37,20 +36,12 @@ int main()
     
     auto path = generator.findPath({x1,y1}, {x2,y2});
 
-    for(auto& coordinate : path) {
-        std::cout << "("<< coordinate.x << "," << coordinate.y << ") -> ";
-    }
+    //for(auto& coordinate : path) {
+     //  std::cout << "("<< coordinate.x << "," << coordinate.y << ") -> ";
+    //}
 
 
-gettimeofday(&stopTime, nullptr);
-    double duration = (stopTime.tv_sec - startTime.tv_sec) + (stopTime.tv_usec - startTime.tv_usec) / 1e6;
 
-    std::cout << "Time taken by function: " << duration << " seconds\n";
 
-   /*clock_t stop = clock();
-  
-    double duration = (double)(stop - start) / CLOCKS_PER_SEC;
-
-    std::cout << "Time taken by function: " << duration << " seconds\n";
-     std::cout<<"\n";*/
+   
 }
